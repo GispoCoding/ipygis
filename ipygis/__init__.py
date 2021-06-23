60ed09c249e46ad171544ae675dfad0f7dc1ed8c
Riku Oja <riku.oja@kapsi.fi>
2021-06-23 15:23:34 +0300

    Add missing get_h3_map import

--- ipygis/__init__.py
+++ ipygis/__init__.py
@@ -1,2 +1,2 @@
 from .db_utils import get_connection_url
-from .gis_utils import get_map, generate_map, QueryResult, to_gdf
+from .gis_utils import get_map, get_h3_map, generate_map, QueryResult, to_gdf
